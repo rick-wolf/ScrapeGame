b48eae69b7a23616d71b922b53971116f739e6e8
Rick Wolf <rickwolf@Administrators-MacBook-Air-7.local>
2017-03-29 17:04:36 -0700

    created a constants file

---
+++ constants.py
@@ -0,0 +1,10 @@
+## Super Global
+# metacritic/beautifulsoup stuff
+user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
+headers = {'User-Agent':user_agent,}
+
+sleeptime = 2 # the duration of sleep between requests, in seconds
+
+
+## Games Table
+games_table_url = "http://www.metacritic.com/browse/games/release-date/available/pc/date?view=condensed&page=" # iterate through page numbers
