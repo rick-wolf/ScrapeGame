2ecf24152a5df260735e20e368e9b40bac6f0e6e
Rick Wolf <rickwolf@Administrators-MacBook-Air-7.local>
2017-04-18 08:56:06 -0700

    updated constants

--- constants.py
+++ constants.py
@@ -10,5 +10,5 @@ sleeptime = 2 # the duration of sleep between requests, in seconds
 games_table_url = "http://www.metacritic.com/browse/games/release-date/available/pc/date?view=condensed&page=" # iterate through page numbers
 
 ## Reviews Table
-reviews_table_url = "http://www.metacritic.com/game/pc/"
+reviews_table_url = "http://www.metacritic.com"
 reviews_table_suffix = "/user-reviews"
